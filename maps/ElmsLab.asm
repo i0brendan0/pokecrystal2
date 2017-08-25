e5b0fbb3a56225719dcde4f2a20ede3782cc9680
i0brendan0 <19826742+i0brendan0@users.noreply.github.com>
2017-08-24 20:52:00 -0500

    Give 10 pokeballs

--- maps/ElmsLab.asm
+++ maps/ElmsLab.asm
@@ -507,7 +507,7 @@ AideScript_GiveYouBalls:
 	buttonsound
 	itemtotext POKE_BALL, $1
 	scall AideScript_ReceiveTheBalls
-	giveitem POKE_BALL, 5
+	giveitem POKE_BALL, 10
 	writetext AideText_ExplainBalls
 	buttonsound
 	itemnotify
