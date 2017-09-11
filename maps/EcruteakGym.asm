bbecf35ec261ce53dceb8ff1cb3c1c79c6dc24ad
i0brendan0 <19826742+i0brendan0@users.noreply.github.com>
2017-09-11 18:29:25 -0500

    10 TMs

--- maps/EcruteakGym.asm
+++ maps/EcruteakGym.asm
@@ -57,7 +57,7 @@ MortyScript_0x99d58:
 	setevent EVENT_BEAT_MEDIUM_GRACE
 	writetext UnknownText_0x9a059
 	buttonsound
-	verbosegiveitem TM_SHADOW_BALL
+	verbosegiveitem TM_SHADOW_BALL, 10
 	iffalse UnknownScript_0x99db5
 	setevent EVENT_GOT_TM30_SHADOW_BALL
 	writetext UnknownText_0x9a0ec
