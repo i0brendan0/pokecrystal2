f966760ece31ed6f4fdf5d2978a16eb2693e0419
i0brendan0 <19826742+i0brendan0@users.noreply.github.com>
2017-09-11 19:38:48 -0500

    10 TMs

--- maps/VioletGym.asm
+++ maps/VioletGym.asm
@@ -40,7 +40,7 @@ FalknerScript_0x683c2:
 	specialphonecall SPECIALCALL_ASSISTANT
 	writetext UnknownText_0x685c8
 	buttonsound
-	verbosegiveitem TM_MUD_SLAP
+	verbosegiveitem TM_MUD_SLAP, 10
 	iffalse .NoRoomForMudSlap
 	setevent EVENT_GOT_TM31_MUD_SLAP
 	writetext UnknownText_0x68648
