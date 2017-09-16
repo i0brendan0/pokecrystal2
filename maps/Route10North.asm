3bae8ffc0b4e9f766d9383ff481f7a5995ee029f
i0brendan0 <19826742+i0brendan0@users.noreply.github.com>
2017-09-15 21:15:19 -0500

    Add Zapdos Event
    No need to move unless in wrong spot

--- maps/Route10North.asm
+++ maps/Route10North.asm
@@ -1,9 +1,47 @@
+const_value set 2
+	const LEGENDARYBIRD_ZAPDOS
+
 Route10North_MapScriptHeader:
 .MapTriggers:
 	db 0
 
 .MapCallbacks:
 	db 0
+	db 1
+
+	dbw MAPCALLBACK_OBJECTS, .Zapdos
+
+.Zapdos:
+	checkevent EVENT_FOUGHT_ZAPDOS
+	iftrue .NoAppear
+	checkevent EVENT_BEAT_RED
+	iftrue .Appear
+	jump .NoAppear
+
+.Appear:
+	appear LEGENDARYBIRD_ZAPDOS
+	return
+
+.NoAppear:
+	disappear LEGENDARYBIRD_ZAPDOS
+	return
+
+Zapdos:
+	opentext
+	writetext ZapdosText
+	cry ZAPDOS
+	pause 15
+	closetext
+	setevent EVENT_FOUGHT_ZAPDOS
+	loadwildmon ZAPDOS, 50
+	starbattle
+	disappear LEGENDARYBIRD_ZAPDOS
+	reloadmapafterbattle
+	end
+
+ZapdosText:
+	text "Gyaoo!"
+	done
 
 PowerPlantSign:
 	jumptext PowerPlantSignText
@@ -33,4 +71,5 @@ Route10North_MapEventHeader:
 	signpost 1, 12, SIGNPOST_READ, Route10PokeCenterSign
 
 .PersonEvents:
-	db 0
+	db 1
+	person_event SPRITE_MOLTRES, 10, 7, SPRITEMOVEDATA_POKEMON, 0, 0, -1, (1 << 3) | PAL_OW_BROWN, PERSONTYPE_SCRIPT, 0, Zapdos, EVENT_LEGENDARY_BIRD_ZAPDOS
