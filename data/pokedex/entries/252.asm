6fd0a0a5b9242dcd0a05709b06c7b65b71575cf4
i0brendan0 <19826742+i0brendan0@users.noreply.github.com>
2017-10-07 01:01:42 -0500

    Create 252.asm

---
+++ data/pokedex/entries/252.asm
@@ -0,0 +1,10 @@
+; MissingNo.PokedexEntry: ; 0x1d18cc
+	db "UNKNOWN@" ; species name
+	dw 100, 100 ; height, width
+
+	db   "Nothing is"
+	next "known about this"
+	next "#MON so far."
+	page "If you are seeing"
+	next "this you cheated"
+	next "at the game.@"
